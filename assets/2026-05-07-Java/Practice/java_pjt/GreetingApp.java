bbaec6393201d4ddcf04655533c401492e41e846
YuiseoYule <yule.yuiseo@gmail.com>
2026-05-07 17:07:52 +0900

    0507 study

---
+++ assets/2026-05-07-Java/Practice/java_pjt/GreetingApp.java
@@ -0,0 +1,7 @@
+public class GreetingApp {  
+
+    public static void main(String args[]) {
+        System.out.println("Hello, java");
+    }
+    
+}
---
+++ assets/2026-05-07-Java/Practice/java_pjt/VariableApp.java
@@ -0,0 +1,19 @@
+public class VariableApp {
+    //Global variable for java, need to be inserted here(in class)
+    public String  name       = "yuiseo" ; //for name, We attribut 'String' so we need to use double quo
+    public int     age        = 25;
+    public double  height     = 172.5 ;
+    public boolean isMarriage = true;
+    public char    gender     = 'm' ; //for gender, we attibute 'char' so we need to use singlequo
+
+    public static void main(String[] args) {
+
+        /*
+        varaibles defined in here are 'local variables'
+        which only can be used in this method block
+        */
+        String msg = " this is starting point of programming" ;
+    }
+}
+
+//and this class doesn't have 'Main' which is starting point of java, so it can be comfiled, but never excuted
